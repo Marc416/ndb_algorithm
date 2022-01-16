97bf648e608444bb8ed64d1407227d36b986e100
Marc416 <juni416@naver.com>
2022-01-16 21:36:32 +0900

    6. 정렬 6-12 두배열의 원소 교체

---
+++ AlgorithmStudy/Part1/6.정렬/6-12  두배열의 원소 교체.py
@@ -0,0 +1,13 @@
+# n, k = map(int, input().split())
+# a = list(map(int, input().split()))
+# b = list(int(input().split()))
+
+a = [1, 2, 5, 4, 3]
+b = [5, 5, 6, 6, 5]
+k = 3
+a_sort = sorted(a)  # 오름차순 정렬
+b_sort = sorted(b, reverse=True)  # 내림차순 정렬
+for i in range(k):
+    a_sort[i], b_sort[i] = b_sort[i], a_sort[i]
+print(a_sort)
+print(b_sort)
